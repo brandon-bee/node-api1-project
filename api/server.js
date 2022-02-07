0dd2709e78af80961ad1c03b403d4d07f724d32d
Brandon B <64279000+brandon-bee@users.noreply.github.com>
2022-02-07 12:11:15 -0800

    created POST endpoint. passing all tests

--- api/server.js
+++ api/server.js
@@ -1,3 +1,30 @@
 // BUILD YOUR SERVER HERE
+const express = require('express');
+const model = require('./users/model');
+const server = express();
+server.use(express.json());
+// ENDPOINTS
+server.get('/', (req, res) => {
+  res.json('hello world! -server');
+});
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+server.post('/api/users', (req, res) => {
+  let body = req.body;
+  if (!body.name || !body.bio) {
+    res.status(400).json({
+      message: "Please provide name and bio for the user"
+    });
+  } else {
+    model.insert(body)
+      .then(user => {
+        res.status(201).json(user);
+      })
+      .catch(() => {
+        res.status(500).json({
+          message: "There was an error while saving the user to the database"
+        });
+      });
+  }
+});
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
